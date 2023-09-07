402728be67c42cf7013840d3205841c594651614
aryanmundra <developer.aryann@gmail.com>
2023-09-07 17:54:18 +0530

    first commit

---
+++ EncodeGenerator.py
@@ -0,0 +1,58 @@
+import cv2
+import face_recognition
+import pickle
+import os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://face-project-81975-default-rtdb.firebaseio.com/",
+    'storageBucket': "face-project-81975.appspot.com"
+})
+
+# Importing student images
+folderPath = 'Images'
+pathList = os.listdir(folderPath)
+print(pathList)
+imgList = []
+studentIds = []
+for path in pathList:
+    img = cv2.imread(os.path.join(folderPath, path))
+    if img is not None:
+        imgList.append(img)
+        studentIds.append(os.path.splitext(path)[0])
+
+        fileName = f'{folderPath}/{path}'
+        bucket = storage.bucket()
+        blob = bucket.blob(fileName)
+        blob.upload_from_filename(fileName)
+
+        print(f'Uploaded: {fileName}')
+    else:
+        print(f'Error loading image: {path}')
+
+print(studentIds)
+
+def findEncodings(imagesList):
+    encodeList = []
+    for img in imagesList:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)
+        if len(encode) > 0:
+            encodeList.append(encode[0])
+        else:
+            print(f'Error encoding face in image')
+    return encodeList
+
+print("Encoding Started ...")
+encodeListKnown = findEncodings(imgList)
+encodeListKnownWithIds = [encodeListKnown, studentIds]
+print("Encoding Complete")
+
+file = open("EncodeFile.p", 'wb')
+pickle.dump(encodeListKnownWithIds, file)
+file.close()
+print("File Saved")
